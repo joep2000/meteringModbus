57a6e931db1b02ee8703e2be7f7fd02c548a9770
Joep Niermeijer <j.niermeijer@gmail.com>
2023-07-24 17:08:54 +0100

    commit after crash

--- read_energy_meter.py
+++ read_energy_meter.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient
 from datetime import datetime, timedelta
 from os import path
 import sys
@@ -11,81 +11,67 @@ import yaml
 import logging
 import json
 import paho.mqtt.client as mqtt
+from itertools import count
+import time
+
 
 # Change working dir to the same dir as this script
 os.chdir(sys.path[0])
 
 config = {
-    'mqtt_host': os.environ.get('MQTT','192.168.178.2'),
+    'mqtt_host': os.environ.get('MQTT','192.168.2.2'),
     'mqtt_user': os.environ.get('MQTT_USER',None),
     'mqtt_pass': os.environ.get('MQTT_PASS',None),
     'mqtt_prefix': os.environ.get('MQTT_PREFIX','WP'),
     'mqtt_client': os.environ.get('MQTT_CLIENT','MBmeter'),
 }
 
-class DataCollector(mqtt.Client):
-    def __init__(self, influx_client, meter_yaml, user = config['mqtt_user'], password=config['mqtt_pass'],server = config['mqtt_host'], prefix = config['mqtt_prefix'],client = config['mqtt_client']):
+def mqtt_on_connect(client, userdata, flags, rc):
+    log.info("MQTT connected!")
+    client.subscribe("Verwarming/heating/State", 0)
 
+def mqtt_on_message(client, userdata, msg):
+    if msg.topic == "Verwarming/heating/State":
+        if msg.payload == b'1':
+            collector.Heating = True
+        else:
+            collector.Heating = False
+    #if self.debug:
+    log.info("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload)+ " | heating= %s" % collector.Heating)
+
+class DataCollector():
+    def __init__(self, influx_client, meter_yaml, mqtt_client, instrument_client, prefix = config["mqtt_prefix"]):
+
+        self.Heating = True
         self.influx_client = influx_client
         self.meter_yaml = meter_yaml
         self.max_iterations = None  # run indefinitely by default
         self.meter_map = None
         self.meter_map_last_change = -1
 
+        self.mqtt_client = mqtt_client
+        self.prefix = prefix
+
         self.Energy = 0
         self.TotalEnergy = 0
         self.PrevEnergy = 0
         self.HeatingEnergy = 0
         self.WaterEnergy = 0
-        self.Heating = True
-        self.prefix = prefix
+
+        self.instrument = instrument_client
+    
 
         log.info('Meters:')
         for meter in sorted(self.get_meters()):
             log.info('\t {} <--> {}'.format( meter['id'], meter['name']))
 
-        #Init and connect to MQTT server
-        mqtt.Client.__init__(self,client)
-        self.will_set( topic = "system/" + self.prefix, payload="Offline", qos=1, retain=True)
-
-        if user != None:
-            self.username_pw_set(user,password)
-
-        self.on_connect = self.mqtt_on_connect
-        self.on_message = self.mqtt_on_message
-
-        self.connect(server,keepalive=10)
-        self.publish(topic = "system/"+ self.prefix, payload="Online", qos=1, retain=True)
-
-        self.loop_start()
-
     def SendMeterEvent(self,power,energy,heating,water):
-
         topic = self.prefix+"/meterevent"
-
         msg = json.dumps({"power":power,"energy":energy,"heating":heating,"water":water})
-
-        self.publish(topic,msg,1)
-
+        self.mqtt_client.publish(topic,msg,1)
         log.info('MQTT published energy: %.3f' % float(energy) )
-
         return
 
-
-    def mqtt_on_connect(self, client, userdata, flags, rc):
-        log.info("MQTT connected!")
-        self.subscribe("Verwarming/heating/State", 0)
-
-    def mqtt_on_message(self, client, userdata, msg):
-        if msg.topic == "Verwarming/heating/State":
-            if str(msg.payload) == "1":
-                self.Heating = True
-            else:
-                self.Heating = False
-        #if self.debug:
-        log.info("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
-
-        return
 
     def get_meters(self):
         assert path.exists(self.meter_yaml), 'Meter map not found: %s' % self.meter_yaml
@@ -101,32 +87,30 @@ class DataCollector(mqtt.Client):
         return self.meter_map
 
     def collect_and_store(self):
-        #instrument.debug = True
+        # self.instrument.debug = True
         meters = self.get_meters()
         t_utc = datetime.utcnow()
         t_str = t_utc.isoformat() + 'Z'
 
-        instrument = minimalmodbus.Instrument('/dev/ttyUSB1', 1) # port name, slave address (in decimal)
-        instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
         datas = dict()
         meter_id_name = dict() # mapping id to name
 
         for meter in meters:
             meter_id_name[meter['id']] = meter['name']
-            instrument.serial.baudrate = meter['baudrate']
-            instrument.serial.bytesize = meter['bytesize']
+            self.instrument.serial.baudrate = meter['baudrate']
+            self.instrument.serial.bytesize = meter['bytesize']
             if meter['parity'] == 'none':
-                instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
+                self.instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
             elif meter['parity'] == 'odd':
-                instrument.serial.parity = minimalmodbus.serial.PARITY_ODD
+                self.instrument.serial.parity = minimalmodbus.serial.PARITY_ODD
             elif meter['parity'] == 'even':
-                instrument.serial.parity = minimalmodbus.serial.PARITY_EVEN
+                self.instrument.serial.parity = minimalmodbus.serial.PARITY_EVEN
             else:
                 log.error('No parity specified')
                 raise
-            instrument.serial.stopbits = meter['stopbits']
-            instrument.serial.timeout  = meter['timeout']    # seconds
-            instrument.address = meter['id']    # this is the slave address number
+            self.instrument.serial.stopbits = meter['stopbits']
+            self.instrument.serial.timeout  = meter['timeout']    # seconds
+            self.instrument.address = meter['id']    # this is the slave address number
 
             log.debug('Reading meter %s.' % (meter['id']))
             start_time = time.time()
@@ -145,7 +129,7 @@ class DataCollector(mqtt.Client):
                            datas[meter['id']][parameter] = instrument.read_float(parameters[parameter], 3, 2, 0)
                         else:
                            datas[meter['id']][parameter] = instrument.read_float(parameters[parameter], 4, 2)
-                           # log.debug('parameter: %s.' % datas[meter['id']][parameter])
+                          # log.debug('parameter: %s.' % datas[meter['id']][parameter])
                         retries = 0
                         pass
                     except ValueError as ve:
@@ -171,6 +155,7 @@ class DataCollector(mqtt.Client):
                         raise
 
             datas[meter['id']]['Read time'] =  time.time() - start_time
+            log.debug("read time: %s",datas[meter['id']]['Read time'])
 
         json_body = [
             {
@@ -186,19 +171,22 @@ class DataCollector(mqtt.Client):
         ]
         if len(json_body) > 0:
             try:
-                self.influx_client.write_points(json_body)
-                log.info(t_str + ' Data written for %d meters.' % len(json_body))
+                with self.influx_client.write_api() as write_api:
+                    write_api.write("db_meters/autogen", "thuis", json_body)
+                log.debug(t_str + ' Data written for %d meters.' % len(json_body))
+                log.debug("heating: %s" % self.Heating)
                 
-                Power = datas[2]['Active power Phase 1']
+                Power = datas[1]['Total system power']
                 if self.PrevEnergy != 0:
-                    self.Energy = (datas[2]['Import active energy'] - self.PrevEnergy) * 1000
-                self.PrevEnergy = datas[2]['Import active energy']
+                    self.Energy = (datas[1]['Import active energy'] - self.PrevEnergy) * 1000
+                self.PrevEnergy = datas[1]['Import active energy']
                 self.TotalEnergy += self.Energy
 
                 if self.Heating == True:
                     self.HeatingEnergy += self.Energy
                 if self.Heating == False:
                     self.WaterEnergy += self.Energy
+                    # log.debug("heating is indeed %s" % self.Heating)
                 self.SendMeterEvent(str(Power),str(self.TotalEnergy),str(self.HeatingEnergy),str(self.WaterEnergy))
 
             except Exception as e:
@@ -210,8 +198,6 @@ class DataCollector(mqtt.Client):
 
 
 def repeat(interval_sec, max_iter, func, *args, **kwargs):
-    from itertools import count
-    import time
     starttime = time.time()
     for i in count():
         if interval_sec > 0:
@@ -231,8 +217,8 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--interval', default=60,
-                        help='Meter readout interval (seconds), default 60')
+    parser.add_argument('--interval', default=30,
+                        help='Meter readout interval (seconds), default 30')
     parser.add_argument('--meters', default='meters.yml',
                         help='YAML file containing Meter ID, name, type etc. Default "meters.yml"')
     parser.add_argument('--log', default='CRITICAL',
@@ -256,21 +242,44 @@ if __name__ == '__main__':
         loghandle = logging.StreamHandler()
 
     log.addHandler(loghandle)
-
     log.info('Started app')
 
+    
     # Create the InfluxDB object
     influx_config = yaml.load(open('influx_config.yml'), Loader=yaml.FullLoader)
-    client = InfluxDBClient(influx_config['host'],
-                            influx_config['port'],
-                            influx_config['user'],
-                            influx_config['password'],
-                            influx_config['dbname'])
+    client = InfluxDBClient(url=influx_config['host'],
+                            token=influx_config['token'],
+                            org=influx_config['org'])
 
+    
+    #Init and connect to MQTT server
+    mqtt_client = mqtt.Client()
+    mqtt_client.will_set(topic = "system/" + config["mqtt_prefix"], payload="Offline", qos=1, retain=True)
+    user = config["mqtt_user"]
+    password = config["mqtt_pass"]
+    if user != None:
+        mqtt_client.username_pw_set(user,password)
+    mqtt_client.on_connect = mqtt_on_connect
+    mqtt_client.on_message = mqtt_on_message
+
+    mqtt_client.connect(config["mqtt_host"],keepalive=10)
+    mqtt_client.publish(topic = "system/"+ config["mqtt_prefix"], payload="Online", qos=1, retain=True)
+    mqtt_client.loop_start()
+
+    instrument = minimalmodbus.Instrument('/dev/ttyUSB1', 1) # port name, slave address (in decimal)
+    instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii modei
+    
+    
     collector = DataCollector(influx_client=client,
-                              meter_yaml=args.meters)
+                              meter_yaml=args.meters,mqtt_client=mqtt_client, instrument_client=instrument)
 
-    repeat(interval,
+
+    try:
+        repeat(interval,
            max_iter=collector.max_iterations,
            func=lambda: collector.collect_and_store())
-
+    except Exception as e:
+        log.error(e)
+        instrument.serial.close()
+        mqtt_client.loop_stop()
+        client.close()
